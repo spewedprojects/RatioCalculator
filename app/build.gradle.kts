0a91c73ee4130f1debf08ad963441d0a4c82e2bd
Rajesh <rkharat98@live.com>
2026-02-12 00:14:13 +0530

    Migrated from Gradle Client to Daemon and upgraded various dependencies - **Upgrades:**  - AGP from v8.10.1 to v9.0.0  - Kotlin to v2.3.10  - Material library to v1.13.0  - AppCompatActivity to v1.12.4
    - Added current version string resource in build.gradle.kts to pass on to the textview below GitHub Icon.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -11,8 +13,15 @@ android {
         applicationId = "com.gratus.ratiocalculator"
         minSdk = 28
         targetSdk = 36
-        versionCode = 8
-        versionName = "4.0.0"
+        versionCode = 9
+        versionName = "5.0.0"
+
+        // Pass versionName to the app as a resource
+        resValue(
+            type = "string",
+            name = "app_version",
+            value = "v" + versionName!! + " (" + versionCode!! + ")"
+        )
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -32,9 +41,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
+
 }
 
 dependencies {
